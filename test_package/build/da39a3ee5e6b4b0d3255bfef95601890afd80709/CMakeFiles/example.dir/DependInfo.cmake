e790362bacf2d5d3acea801f10fa2ff619b19900
iblis-ms <marcin.serwach@gmail.com>
2017-04-23 10:24:52 +0200

    Start work :)

---
+++ test_package/build/da39a3ee5e6b4b0d3255bfef95601890afd80709/CMakeFiles/example.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/iblis/workspaces/github/conan_gbenchmark/test_package/GBenchmarkExample.cpp" "/home/iblis/workspaces/github/conan_gbenchmark/test_package/build/da39a3ee5e6b4b0d3255bfef95601890afd80709/CMakeFiles/example.dir/GBenchmarkExample.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "_GLIBCXX_USE_CXX11_ABI=1"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/iblis/.conan/data/GBenchmark/master/iblis_ms/stable/package/81607951755e61cf17149d40bed6aba5b3a079a9/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
